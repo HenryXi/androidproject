09d136a0416ae6ea0037954ccaf35d6f8c9e25bd
yong <xixiaoyong@xiaomi.com>
2022-05-30 14:32:47 +0800

    this is auto commit

---
+++ chapter03/ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.kt
@@ -0,0 +1,77 @@
+package com.example.activitytest
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.first_layout.*
+
+class FirstActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.first_layout)
+//        button1.setOnClickListener {
+//            Toast.makeText(this, "You clicked Button 1", Toast.LENGTH_SHORT).show()
+//            finish()
+//        }
+//        button1.setOnClickListener {
+//            val intent = Intent(this, SecondActivity::class.java)
+//            startActivity(intent)
+//        }
+
+//        button1.setOnClickListener {
+//            val intent = Intent("com.example.activitytest.ACTION_START")
+//            intent.addCategory("com.example.activitytest.MY_CATEGORY")
+//            startActivity(intent)
+//        }
+
+//        button1.setOnClickListener {
+//            val intent = Intent(Intent.ACTION_VIEW)
+//            intent.data = Uri.parse("https://www.baidu.com")
+//            startActivity(intent)
+//        }
+
+//        button1.setOnClickListener {
+//            val intent = Intent(Intent.ACTION_DIAL)
+//            intent.data = Uri.parse("tel:10086")
+//            startActivity(intent)
+//        }
+
+//        button1.setOnClickListener {
+//            val data = "Hello SecondActivity"
+//            val intent = Intent(this, SecondActivity::class.java)
+//            intent.putExtra("extra_data", data)
+//            startActivity(intent)
+//        }
+
+        button1.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivityForResult(intent, 1)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) { super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            1 -> if (resultCode == RESULT_OK) {
+                val returnedData = data?.getStringExtra("data_return")
+                Log.d("FirstActivity", "returned data is $returnedData")
+            } }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add_item -> Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show()
+            R.id.remove_item -> Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show()
+        }
+        return true
+    }
+}
